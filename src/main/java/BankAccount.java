850472569ec4096ef7a0550e04f175b09b417a78
Zaneta Szustakiewicz <netka7@hotmail.com>
2021-06-20 09:22:44 +0200

    new account

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,35 @@
+public class BankAccount {
+    private int accountBalance;
+
+    public BankAccount(){
+   accountBalance = 0;
+    }
+
+//konstruktor parametrowy
+public BankAccount (int accountBalance){
+    this.accountBalance = accountBalance;
+
+
+}
+
+public boolean withdraw(int amount){
+    if (amount > this.accountBalance) {
+        return false;
+    }this.accountBalance = accountBalance - amount;
+    return true;
+
+    }
+    //Metoda druga
+    public void transfer(int amount){
+    this.accountBalance = accountBalance + amount;
+
+    }
+
+    public int getAccountBalance() {
+        return this.accountBalance;
+    }
+}
+
+
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount mBank = new BankAccount();  // nowe konto bankowe z nazwa Mbank
+        mBank.transfer(1000);  //włacenie kasy na konto Mbank
+        mBank.withdraw(1000);
+        mBank.transfer(200);
+        mBank.getAccountBalance();
+
+
+    }
+
+}
